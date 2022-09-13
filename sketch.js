d1b4244a8e6812c9bd848862dbc2624250567cb0
Himanshu Dongre <hdongr2@uic.edu>
2022-09-13 08:36:30 -0500

    Animated block

--- sketch.js
+++ sketch.js
@@ -8,14 +8,13 @@ const blockSpeedY = 1;
 function setup() 
 {
     createCanvas(windowWidth, windowHeight);
-    background(0);
 }
 
 function draw()
 {
     background(0);
     fill(255, 255, 255);
-    rect(blockX, blockY, blockWidh, blockHeight);
+    rect(blockX, blockY, blockWidth, blockHeight);
     blockX = blockX + blockSpeedX;
     if (blockX > width || blockX < 0) {
         blockSpeedX *= -1;
