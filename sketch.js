bb540ac78d3ad646da3003619af4bbfcd53a96cf
Himanshu Dongre <hdongr2@uic.edu>
2022-09-17 05:53:19 -0500

    Added color

--- sketch.js
+++ sketch.js
@@ -10,9 +10,17 @@ class Ball
         this.gray = gray;
     }
 
-    display()
+    display(col)
     {
-        fill(this.gray);
+        if (col == 1) {
+            fill(this.gray, 0, 0);
+        } else if (col == 2) {
+            fill(0, this.gray, 0);
+        } else if (col == 3) {
+            fill(0, 0, this.gray);
+        } else {
+            fill(this.gray);
+        }
         circle(this.x, this.y, this.radius);
     }
 
@@ -20,63 +28,47 @@ class Ball
     {
         this.x += this.speed_x;
         this.y += this.speed_y;
-        if (this.x >= (width - this.radius) || this.x <= this.radius ||
-            this.y >= (height - this.radius) || this.y <= this.radius) {
+        this.gray += 25;
+        if (this.gray >= 255) {
+            this.gray = 0;
+        }
+        if (this.x >= (width - this.radius) || this.x <= this.radius) {
             this.speed_x *= -1;
         }
+        if (this.y >= (height - this.radius) || this.y <= this.radius) {
+            this.speed_y *= -1;
+        }
     }
 
-    mad_move()
-    {
-        this.x += this.speed_x;
-        this.y += this.speed_y;
-        if (this.x >= (width - this.radius) || this.x <= this.radius) {
-            if (this.speed_x > 0) {
-                this.speed_x = -random(2, 7);
-            } else {
-                this.speed_x = random(2,7); 
-            }
-            this.gray = random(256)
-            --this.radius;
-        }
-        if (this.y >= (height - this.radius) || this.y <= this.radius) {
-            if (this.speed_y > 0) {
-                this.speed_y = -random(2, 7);
-            } else {
-                this.speed_y = random(2, 7); 
-            }
-            this.gray = random(256);
-            --this.radius;
-        }   
-    }
 }
 
-const r = 10
+const r = 5;
 let balls = [];
-let N = 200;
+let N = 16000;
 
 function setup() 
 {
     createCanvas(windowWidth, windowHeight);
     background(0);
+    frameRate(1000);
     for (let j = 0; j < N / 4; ++j) {
-        balls[j] = new Ball(width / 2, height / 2, r, 2 - (8 * j / N), 8 * j / N, 255);
+        balls[j] = new Ball(width / 2, height / 2, r, 4 - (16 * j / N), 16 * j / N, 0);
     }
     for (let j = N / 4; j < N / 2; ++j) {
-        balls[j] = new Ball(width / 2, height / 2, r, -2 + (8 * (j - (N / 4)) / N), -8 * (j - (N / 4)) / N, 255);
+        balls[j] = new Ball(width / 2, height / 2, r, -4 + (16 * (j - (N / 4)) / N), -16 * (j - (N / 4)) / N, 0);
     }
     for (let j = N / 2; j < 3 * (N / 4); ++j) {
-        balls[j] = new Ball(width / 2, height / 2, r, 2 - (8 * (j - (N / 2)) / N), -8 * (j - (N / 2)) / N, 255);
+        balls[j] = new Ball(width / 2, height / 2, r, 4 - (16 * (j - (N / 2)) / N), -16 * (j - (N / 2)) / N, 0);
     }
     for (let j = 3 * (N / 4); j < N; ++j) {
-        balls[j] = new Ball(width / 2, height / 2, r, -2 + (8 * (j - (3 * N / 4)) / N), 8 * (j - (3 * N / 4)) / N, 255);
+        balls[j] = new Ball(width / 2, height / 2, r, -4 + (16 * (j - (3 * N / 4)) / N), 16 * (j - (3 * N / 4)) / N, 0);
     }
 }
 
 function draw()
 {
     for (let i = 0; i < N; i++) {
-        balls[i].display();
-        balls[i].mad_move();
+        balls[i].display(2);
+        balls[i].move();
     }
 }
